c4a8d86513f2c94bfae7b8ae1d10e831de5cff14
Simon Evans <simon.evans@torchbox.com>
2017-12-19 23:56:02 +0000

    Strip out headings

--- src/components/Home/Introduction/Introduction.js
+++ src/components/Home/Introduction/Introduction.js
@@ -1,18 +1,11 @@
 import React, { Component } from 'react'
-import { Link } from 'react-router'
 import './Introduction.css'
 
 class Introduction extends Component {
   render() {
     return (
       <div className="introduction">
-        <h1 className="animate-in">Dipsum sipsum</h1>
-        <h2 className="animate-in">
-          Lorem ipsum dolor sit amet.
-        </h2>
-        <h2 className="animate-in">
-          <Link to='/about'>Page link.</Link>
-        </h2>
+        <h1 className="animate-in-ui">Dipsum Sipsum</h1>
       </div>
     )
   }
