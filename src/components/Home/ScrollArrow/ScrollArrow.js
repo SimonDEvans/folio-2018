ad54dbb18c501a45e17a326bc4d219b87976a331
Simon Evans <simon.evans@torchbox.com>
2017-12-23 23:04:25 +0000

    Update scroll arrow style

--- src/components/Home/ScrollArrow/ScrollArrow.js
+++ src/components/Home/ScrollArrow/ScrollArrow.js
@@ -6,15 +6,21 @@ import Scrollchor from 'react-scrollchor'
 class ScrollArrow extends Component {
   componentDidMount() {
     setTimeout(() => {
-      document.getElementById('scroll-arrow').className += ' scroll-arrow--show'
-    }, 700);
+      document.getElementById('scroll-arrow').className += ' c-scroll-icon--show'
+    }, 700)
   }
 
   render() {
     return (
       <Parallax className="scroll-arrow-container" offsetYMin={-1500}>
-        <Scrollchor to="#about" animate={{offset: -100, duration: 800}} id="scroll-arrow" className="scroll-arrow">
-          <span></span>
+        <Scrollchor id="scroll-arrow" to="#about" animate={{offset: -100, duration: 800}} className="c-scroll-icon">
+          <div className="c-scroll-icon-line-mask"><div className="c-scroll-icon-line"></div></div>
+          <div className="c-scroll-icon-triangle">
+            <div className="c-scroll-icon-triangle-mask first"><div className="c-scroll-icon-triangle-line first"></div></div>
+            <div className="c-scroll-icon-triangle-mask right"><div className="c-scroll-icon-triangle-line right"></div></div>
+            <div className="c-scroll-icon-triangle-mask left"><div className="c-scroll-icon-triangle-line left"></div></div>
+            <div className="c-scroll-icon-triangle-mask last"><div className="c-scroll-icon-triangle-line last"></div></div>
+          </div>
         </Scrollchor>
       </Parallax>
     )
